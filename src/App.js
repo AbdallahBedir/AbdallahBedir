b271de2c4937a470fc04dead4b7e3a8f5bf80427
AbdallahBedir <abdallahahmed2025@gmail.com>
2020-07-17 00:36:12 +0200

    [Chore] Add prettier

--- src/App.js
+++ src/App.js
@@ -1,65 +1,66 @@
-import React from 'react';
-import { ThemeProvider, withStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppLayout from './AppLayout';
-import theme from './theme';
+import React from "react";
+import { ThemeProvider, withStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppLayout from "./AppLayout";
+import theme from "./theme";
 import { CSSTransition } from "react-transition-group";
 
-const GlobalCss = withStyles( (theme) => ({
-    '@global': {
-      '.MuiTypography-body2': {
-        color:theme.palette.text.secondary,
-        fontSize: '12px',
-        marginBottom: '30px',
-        lineHeight: '1.9',
-        fontFamily:theme.typography.fontFamilySecondary
-      },
-      '.box-title': {
-        fontWeight: 'bold',
-        fontSize: '21px',
-        letterSpacing: '2px',
-        marginBottom: theme.spacing(4),
-        textTransform: 'uppercase'
-      },
-      '.MuiFilledInput-root': {
-        borderRadius: '3px'
-      },
-      '.MuiInputBase-input':{
-        fontSize: '14px'
-      }
+const GlobalCss = withStyles((theme) => ({
+  "@global": {
+    ".MuiTypography-body2": {
+      color: theme.palette.text.secondary,
+      fontSize: "12px",
+      marginBottom: "30px",
+      lineHeight: "1.9",
+      fontFamily: theme.typography.fontFamilySecondary,
     },
+    ".box-title": {
+      fontWeight: "bold",
+      fontSize: "21px",
+      letterSpacing: "2px",
+      marginBottom: theme.spacing(4),
+      textTransform: "uppercase",
+    },
+    ".MuiFilledInput-root": {
+      borderRadius: "3px",
+    },
+    ".MuiInputBase-input": {
+      fontSize: "14px",
+    },
+  },
 }))(() => null);
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      mounted: false
+      mounted: false,
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.setState({
-      mounted: true
-    })
+      mounted: true,
+    });
   }
 
-  render(){
+  render() {
     return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <GlobalCss />
-      <CSSTransition
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalCss />
+        <CSSTransition
           in={this.state.mounted}
           appear={true}
           classNames="fade"
-          timeout={2000}>
+          timeout={2000}
+        >
           <div>
-          <AppLayout />
+            <AppLayout />
           </div>
-      </CSSTransition>
-    </ThemeProvider>
-    )
+        </CSSTransition>
+      </ThemeProvider>
+    );
   }
 }
 
